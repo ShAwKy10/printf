5a777118c96ea9dbc76c4fe547a6595aeca2a859
Shawky <amamasa121212@gmail.com>
2023-10-20 13:29:52 -0700

    Updating the _printf.c file

--- _printf.c
+++ _printf.c
@@ -8,44 +8,44 @@
  */
 int _printf(const char *format, ...)
 {
-    int sum = 0;
-    va_list ap;
-    char *p, *start;
-    params_t params = PARAMS_INIT;
+	int sum = 0;
+	va_list ap;
+	char *p, *start;
+	params_t params = PARAMS_INIT;
 
-    va_start(ap, format);
+	va_start(ap, format);
 
-    if (!format || format[0] == '%' && !format[1])
-        return (-1);
-    if (format[0] == '%' && format[1] == ' ' && !format[2])
-        return (-1);
-    for (p = (char *)format; *p; p++)
-    {
-        init_params(ap, &params);
-        if (*p != '%')
-        {
-            sum += _putchar(*p);
-            continue;
-        }
-        start = p;
-        p++;
+	if (!format || format[0] == '%' && !format[1])
+		return (-1);
+	if (format[0] == '%' && format[1] == ' ' && !format[2])
+		return (-1);
+	for (p = (char *)format; *p; p++)
+	{
+		init_params(ap, &params);
+		if (*p != '%')
+		{
+			sum += _putchar(*p);
+			continue;
+		}
+		start = p;
+		p++;
 
-        while (get_flag(p, &params))
-        {
-            p++; /*next char*/
-        }
-        p = get_width(p, ap, &params);
-        p = get_precision(p, ap, &params);
-        if (get_modifier(p, &params))
-            p++;
-        if (!get_specifier(p))
-            sum += print_from_to(start, p,
-                    params.l_modifier || params.h_modifier ? p - 1 : 0);
-        else
-            sum += get_print_func(p, ap, &params);
-    }
+		while (get_flag(p, &params))
+		{
+			p++; /*next char*/
+		}
+		p = get_width(p, ap, &params);
+		p = get_precision(p, ap, &params);
+		if (get_modifier(p, &params))
+			p++;
+		if (!get_specifier(p))
+			sum += print_from_to(start, p,
+				params.l_modifier || params.h_modifier ? p - 1 : 0);
+		else
+			sum += get_print_func(p, ap, &params);
+	}
 
-    _putchar(BUF_FLUSH);
-    va_end(ap);
-    return (sum);
+	_putchar(BUF_FLUSH);
+	va_end(ap);
+	return (sum);
 }
