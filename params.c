8c56d134ea40528459dd7eee07d6e46dc0f29e03
Shawky <amamasa121212@gmail.com>
2023-10-20 20:50:18 +0300

    Pushing the params.c file

---
+++ params.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * init_params - clears struct fields and reset buf
+ * @ap: the argument pointer
+ * @params: the parameters struct
+ *
+ * Return: void
+ */
+void init_params(va_list ap, params_t *params)
+{
+    params->unsign = 0;
+
+    params->plus_flag = 0;
+    params->space_flag = 0;
+    params-> hashtag_flag = 0;
+    params-> zero_flag = 0;
+    params->minus_flag = 0;
+
+    params->width = 0;
+    params->precision = UINT_MAX;
+
+    params->h_modifier = 0;
+    params->l_modifier = 0;
+    (void)ap;
+}
