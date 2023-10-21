1310f8e13092a396ce834149231c518c1dd21443
Shawky <amamasa121212@gmail.com>
2023-10-20 19:30:27 -0700

    Updating convert_number.c file

--- convert_number.c
+++ convert_number.c
@@ -39,25 +39,25 @@ int print_hex(va_list ap, params_t *params)
  */
 int print_HEX(va_list ap, params_t *params)
 {
-        unsigned long l;
-        int c = 0;
-        char *str;
+	unsigned long l;
+	int c = 0;
+	char *str;
 
-        if (params->l_modifier)
-                l = (unsigned long)va_arg(ap, unsigned long);
-        else if (params->h_modifier)
-                l = (unsigned short int)va_arg(ap, unsigned int);
-        else
-                l = (unsigned int)va_arg(ap, unsigned int);
+	if (params->l_modifier)
+		l = (unsigned long)va_arg(ap, unsigned long);
+	else if (params->h_modifier)
+		l = (unsigned short int)va_arg(ap, unsigned int);
+	else
+		l = (unsigned int)va_arg(ap, unsigned int);
 
-        str = convert(l, 16, CONVERT_UNSIGNED, params);
-        if (params->hashtag_flag && l)
-        {
-                *--str = 'X';
-                *--str = '0';
-        }
-        params->unsign = 1;
-        return (c += print_number(str, params));
+	str = convert(l, 16, CONVERT_UNSIGNED, params);
+	if (params->hashtag_flag && l)
+	{
+		*--str = 'X';
+		*--str = '0';
+	}
+	params->unsign = 1;
+	return (c += print_number(str, params));
 }
 
 /**
@@ -88,9 +88,9 @@ int print_binary(va_list ap, params_t *params)
  */
 int print_octal(va_list ap, params_t *params)
 {
-        unsigned long l;
-        char *str;
-        int c = 0;
+	unsigned long l;
+	char *str;
+	int c = 0;
 
 	if (params->l_modifier)
 		l = (unsigned long)va_arg(ap, unsigned long);
@@ -100,8 +100,8 @@ int print_octal(va_list ap, params_t *params)
 		l = (unsigned int)va_arg(ap, unsigned int);
 	str = convert(l, 8, CONVERT_UNSIGNED, params);
 
-        if (params->hashtag_flag && l)
-                *--str = '0';
-        params->unsign = 1;
-        return (c += print_number(str, params));
+	if (params->hashtag_flag && l)
+		*--str = '0';
+	params->unsign = 1;
+	return (c += print_number(str, params));
 }
